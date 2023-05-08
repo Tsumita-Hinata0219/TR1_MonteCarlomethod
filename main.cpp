0bc5563e8a3a1203332159d9672f4114f2ae13be
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-05-08 16:15:59 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <Novice.h>
+
+const char kWindowTitle[] = "LE2B_20_ツミタ_ヒナタ_";
+
+
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	const int ScreenWidth = 1280;
+	const int ScreenHeight = 720;
+	Novice::Initialize(kWindowTitle, ScreenWidth, ScreenHeight);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
